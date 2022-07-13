864f44832f36a935648775648dd109e767e538bf
Antoaneta Yordanova <antoaneta.yordanova@tutanota.com>
2022-07-13 23:06:42 +0300

    register finished

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,4 +1,5 @@
 import styles from './Register.module.css';
+import { Link } from 'react-router-dom';
 
 const Regitser = () => {
     return( 
@@ -9,6 +10,7 @@ const Regitser = () => {
                 <input type="password" name="password" id="password" placeholder="Password"/>
                 <input type="password" name="re-password" id="re-password" placeholder="Repeat password"/>
                 <button>Register</button>
+                <p>Already registered? <Link to={'/login'}>Login here</Link></p> 
             </form>
         </section>
 
